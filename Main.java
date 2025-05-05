e392731016ab46ad4717e6961cd1f65684146de5
Gabe Vandevere <gvand@udel.edu>
2025-05-04 20:56:28 -0400

    added more into taskbook

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     boolean terminate = false;
+    TaskBook tasks = new TaskBook();
 
     public static void main(String[] args) {
         Helper.clear_screen();
@@ -13,39 +14,55 @@ public class Main {
     }
 
     void start(){
-        while (true) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (!terminate) {
             Helper.clear_screen();
             System.out.println("Enter a selection:");
             System.out.println("1) View deadline tasks");
-            //System.out.println("2) View long-term tasks");
+            //System.out.println("2) View simple tasks");
             //System.out.println("3) View completed tasks");
             //System.out.println("4) Delete a task");
             //System.out.println("5) Add a task");
             //System.out.println("6) Mark task as complete");
             System.out.println("7) Quit the program");
 
-            int input;
-
-            try {
-                input = System.in.read();
-            } catch (IOException e) {
-                System.out.println("An error occurred while reading input: " + e.getMessage());
-                input = -1;
-            } if (input == -1) { return; }
+            String input = scanner.nextLine();
 
             handle_input(input);
 
-            if (terminate) { return; }
-
         }
+
+        scanner.close();
     }
 
-    void handle_input(int input) {
-        if (input == 1) {
-            //view deadlinetasks
-        }
+    void handle_input(String input) {
+        switch (input.trim()){
+            case "1":
+                tasks.display_task_type("DeadLineTask");
+                break;
 
-        else if (input == 7) { terminate = true; }
+            case "2":
+                break;
+
+            case "3":
+                break;
+
+            case "4":
+                break;
+
+            case "5":
+                break;
+
+            case "6":
+                break;
+
+            case "7":
+                terminate = true;
+                break;
+
+
+        }
     }
 
 }
--- TaskBook.java
+++ TaskBook.java
@@ -1,16 +1,43 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class TaskBook {
-    private List<Task> tasks = new ArrayList<>();
+    private List<SimpleTask> tasks = new ArrayList<>();
 
     TaskBook(){}
 
-    public void addTask(Task task){
+    public void addTask(SimpleTask task){
         tasks.add(task);
     }
 
-    public List<Task> getTasks() {
+    public List<SimpleTask> getTasks() {
         return this.tasks;
     }
+
+    public List<SimpleTask> get_task_type(String type) {
+        // pass in "SimpleTask" for simple, and "DeadLineTask" for deadline.
+        List<SimpleTask> correct_tasks = new ArrayList<>();
+
+        for (SimpleTask task : tasks) {
+            if (task.getType().equals(type)) {
+                correct_tasks.add(task);
+            }
+        }
+
+        return correct_tasks;
+    }
+
+    public void display_task_type(String type){
+        List<SimpleTask> tasks = this.get_task_type(type);
+
+        String result = "";
+
+        for (SimpleTask task : tasks) {
+            result += task.getFormattedTask();
+        }
+
+        System.out.println(result);
+    }
+
 }
